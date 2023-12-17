62682121326aad5d2adc6324ff348dbd86897868
Dario <dario.pratola@gmail.com>
2023-12-17 11:43:32 -0500

    Task7

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -7,40 +7,41 @@ def emotion_detector(text_to_analyze):
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     
     response = requests.post(url, json = myobj, headers=header)
-   
-    # Convert the response text into a dictionary using the json library functions
-    formatted_response = json.loads(response.text)
-    
-   # Estrai la lista delle emozioni e i relativi punteggi dal primo elemento della lista
-    if 'emotionPredictions' in formatted_response:
+
+    if response.status_code == 200:
+        # Convert the response text into a dictionary using the json library functions
+        formatted_response = json.loads(response.text)
         predictions = formatted_response['emotionPredictions']
-        if predictions:
-            first_prediction = predictions[0]
-            emotion_data = first_prediction.get('emotion', {})
-            emotions = emotion_data if emotion_data else {}
-        else:
-            emotions = {}
-    else:
-        emotions = {}
+        first_prediction = predictions[0]
+        emotions = first_prediction.get('emotion', {})
+        # Assegna i punteggi alle variabili specifiche
+        anger_score = emotions.get('anger', 0)
+        disgust_score = emotions.get('disgust', 0)
+        fear_score = emotions.get('fear', 0)
+        joy_score = emotions.get('joy', 0)
+        sadness_score = emotions.get('sadness', 0)
 
-    # Assegna i punteggi alle variabili specifiche
-    anger_score = emotions.get('anger', 0)
-    disgust_score = emotions.get('disgust', 0)
-    fear_score = emotions.get('fear', 0)
-    joy_score = emotions.get('joy', 0)
-    sadness_score = emotions.get('sadness', 0)
+        # Calcola la dominanza dell'emozione
+        dominant_emotion = max(emotions, key=lambda x: emotions[x])
 
-    # Calcola la dominanza dell'emozione
-    dominant_emotion = max(emotions, key=lambda x: emotions[x])
+        # Restituisci la struttura richiesta
+        result = {
+            'anger': anger_score,
+            'disgust': disgust_score,
+            'fear': fear_score,
+            'joy': joy_score,
+            'sadness': sadness_score,
+            'dominant_emotion': dominant_emotion
+        }
 
-    # Restituisci la struttura richiesta
-    result = {
-        'anger': anger_score,
-        'disgust': disgust_score,
-        'fear': fear_score,
-        'joy': joy_score,
-        'sadness': sadness_score,
-        'dominant_emotion': dominant_emotion
+    elif response.status_code == 400:
+        result = {
+        'anger': None,
+        'disgust': None,
+        'fear': None,
+        'joy': None,
+        'sadness': None,
+        'dominant_emotion': None
     }
 
     return result
